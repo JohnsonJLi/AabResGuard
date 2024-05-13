91078886b8a032df05cd1e89dbfa633b0b04337d
yuanquan <yuanquan@yizhoucp.com>
2024-05-13 10:49:07 +0800

    字典随机

--- core/src/main/java/com/bytedance/android/aabresguard/obfuscation/ResGuardStringBuilder.java
+++ core/src/main/java/com/bytedance/android/aabresguard/obfuscation/ResGuardStringBuilder.java
@@ -29,10 +29,11 @@ public class ResGuardStringBuilder {
     };
     private static String[] oldAToAll = {
             "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "_", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k",
-            "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"
+            "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",
+            "g8", "f4", "2x", "wi", "u6", "yz", "t20", "_50", "lw", "wdz", "nr", "lqe", "7d", "faf", "rzs", "boa", "rr", "ur", "r4m", "9h", "_7", "ia", "_q", "a2", "zy", "ge", "bp", "upa", "69d", "5w3", "foz", "fxx", "_rg", "h1", "9y", "xn5", "i8", "ft", "q4", "97", "l69", "k6", "af0", "dw", "0j0", "0a", "80", "c7", "_4", "30q", "qt", "wxd", "h3s", "sap", "3d", "pbx", "39l", "cq", "3q", "_r5", "xw8", "5lo", "cu", "xd4", "gon", "sd", "rg", "9u", "m4", "crw", "o8", "1n", "23", "nt", "ok", "pg", "xoj", "7_f", "gb6", "7l", "9p", "rl", "r9", "c_c", "12", "yb", "rlr", "eh0", "p5", "z8", "3k", "t8", "ho", "br", "r3", "bw", "ib", "6oy", "ucb", "kw", "aaz", "os7", "1ko", "var", "iee", "lc3", "dd", "xk", "2fv", "8s", "b5y", "md", "vi", "0ky", "cx8", "cm", "xuo", "px", "jz", "oe", "4q2", "eny", "v77", "f1", "5_n", "d6", "etc", "spz", "gp", "6m", "g_c", "nw", "vu5", "adv", "hi", "j18", "65", "t2", "uz", "ig_", "hyq", "4um", "xzf", "yt", "jv5", "di", "54", "ahb", "6a", "ick", "8bz", "7x", "uo", "d1t", "_sh", "y6", "hd", "zu", "u9n", "n_g", "kj", "hkp", "cf", "1r1", "6bd", "gx", "x9j", "996", "5id", "4hz", "6g", "p66", "4t", "3e", "ps", "t7g", "z7q", "xe", "4p", "8aq", "_c", "ya", "gu", "5xx", "44", "l7", "b0", "iw", "uvm", "24m", "r5", "jr", "skb", "_w", "4z", "bf", "4yr", "i9", "ol", "c2", "p9", "7k", "ne6", "0va", "2sd", "6az", "t9s", "po", "l8h", "no", "v0", "dbx", "pi", "bh9", "i_", "r0", "e5d", "37", "v0g", "zur", "ij", "bl6", "gf8", "co", "63o", "p6", "es", "6c", "10u", "4fv", "zw", "cza", "0r", "fh1", "l4p", "99w", "ig", "p3f", "ri", "ert", "pfo", "6zc", "nba", "p9e", "fs", "ug", "j1", "o12", "d5", "qa", "axv", "18", "ul2", "q_", "cx", "2y", "l82", "3b0", "p_u", "p2", "sz9", "mgb", "syk", "b9h", "ei5", "sp", "h14", "86m", "_ls", "j4x", "69s", "9rw", "x6s", "3v_", "gv", "s37", "xc8", "88", "sut", "eod", "5f7", "b19", "1z", "2k", "co0", "gey", "lj8", "jg", "hc", "sf", "85", "fx", "6n6", "0o", "1f", "lf", "zqm", "pxg", "1t", "mu", "4c4", "jw", "op", "5ai", "4a_", "p_", "qxz", "yhs", "q1", "b8x", "hjf", "1nl", "4r", "k5", "9fg", "je6", "62", "fv1", "_wd", "kz", "xa", "mc", "bt", "p3", "kl", "o6q", "0m", "c0", "zp", "tu", "u0k", "bq", "69", "zv7", "xf0", "0k2", "cyp", "n40", "7p7", "nvf", "hvy", "vhe", "ie0", "cen", "g1t", "16j", "9x", "f1x", "4w", "njt", "2p", "tp", "g1l", "546", "ahy", "0u", "8_k", "itb", "kn", "in", "bxt", "lba", "x5", "w0x", "dax", "ns", "n2o", "x8", "w1l", "a68", "tv6", "k3u", "ki9", "pk", "nso", "ply", "vh", "_7b", "z7", "60", "7pd", "c60", "h7", "zj", "o4r", "cmj", "0_", "v3", "y5p", "c8", "zbo", "ck", "vt", "fr", "zd", "udi", "jsz", "uek", "lb", "ykb", "ntc", "ky", "5vt", "a9", "icu", "z5", "_6", "8t", "s0z", "ub", "418", "d9", "by5", "bv", "2h", "e8", "dm", "qlu", "2c", "_qn", "nj6", "6jd", "bg", "zk3", "vz", "m2", "382", "yl", "au", "kh", "wr", "ubj", "b06", "18q", "wxc", "15o", "a5", "dsp", "7eo", "ui", "1f6", "jm", "q5", "yr", "bn", "8im", "fl", "ii", "kmv", "w8", "29", "82d", "b2", "wp8", "pt", "430", "v7j", "bk", "22", "rx", "ld2", "p9m", "h_6", "djy", "7vd", "tn", "mit", "kp", "il", "inc", "lwl", "421", "jte", "5sc", "3r", "tuu", "4go", "ed", "pyh", "9m_", "17", "is", "am", "kr", "rh7", "aw", "tf_", "xtm", "_92", "u4", "hh", "s5", "3we", "re2", "7e", "4_", "soa", "t9j", "4zv", "rd5"
     };
 
-    public static String[] getRandomizedAndTrimmedArray(String[] sourceArray) {
+    public static String[] getRandomizedAndTrimmedArray(String[] sourceArray, int limit) {
         if (sourceArray.length < 20) {
             throw new IllegalArgumentException("Source array must contain at least 20 elements.");
         }
@@ -45,17 +46,17 @@ public class ResGuardStringBuilder {
 
         // Perform deletion while maintaining the constraint
         List<String> tempList = new ArrayList<>(Arrays.asList(arrayCopy));
-        while (tempList.size() > 20 && !tempList.isEmpty()) {
+        while (tempList.size() > limit && !tempList.isEmpty()) {
             tempList.remove(new Random().nextInt(tempList.size()));
         }
 
-        String[] array = tempList.subList(0, Math.min(tempList.size(), 20)).toArray(new String[0]);
+        String[] array = tempList.subList(0, Math.min(tempList.size(), limit)).toArray(new String[0]);
         System.out.println("Random arg : " + Arrays.toString(array));
         return array;
     }
 
-    private static String[] mAToZ = getRandomizedAndTrimmedArray(oldAToZ);
-    private static String[] mAToAll = getRandomizedAndTrimmedArray(oldAToAll);
+    private static String[] mAToZ = getRandomizedAndTrimmedArray(oldAToZ, 20);
+    private static String[] mAToAll = getRandomizedAndTrimmedArray(oldAToAll, 80);
 
     /**
      * 在window上面有些关键字是不能作为文件名的
@@ -90,7 +91,7 @@ public class ResGuardStringBuilder {
         for (String first : mAToZ) {
             for (String aMAToAll : mAToAll) {
                 String str = first + aMAToAll;
-                if (!Utils.match(str, blacklistPatterns)) {
+                if (!mFileNameBlackList.contains(str) | !Utils.match(str, blacklistPatterns)) {
                     mReplaceStringBuffer.add(str);
                 }
             }
--- core/src/test/java/com/bytedance/android/aabresguard/RandomArraySelectorTest.java
+++ core/src/test/java/com/bytedance/android/aabresguard/RandomArraySelectorTest.java
@@ -14,7 +14,7 @@ public class RandomArraySelectorTest {
     };
     private String[] mAToAll = {
             "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "_", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k",
-            "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"
+            "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z","f1t","v","re","c7","qc","pf","se","p","bb","s","_","j9","8s","sb","g","_jj","pag","84","s4i","8r","4mr","e","3m","9","nnb","8g","yd3","si6","ce","hoe","5","po","hgg","r2","su","mwu","8","ufn","0","icw","of","i","x","ld3","u","q","1","64","71","iw","q3o","xf","ju","p4","j","5i","q4","4ae","f","meh","m","a","cce","w","h2u","kev","2u","zv","hb","dum","6","05_","uk","j7","4w","b3","_8","0f","o","a_","wl","7i","b","y4d","t","ipp","otv","78p","2p","k","87","9d","jr","e04","axk","q3","zah","4hf","s_","d","2","pr","ecf","2e","c4w","h","9j","de","d0","sp","1ms","9s","xc","04u","nk","kc","a8","mj","op","f6","fo","cy","fc","g7","ra","ihi","6c","0gw","s6w","u_","feu","95p","zci","pb5","mkf","wf4","r","yxk","j4o","c80","14","uo","_qk","qu","7y","xi0","tk","kq","y","0a","2v","fh","tq9","v46","3","58","as4","km","4d7","ai","nz","a32","noa","bm","ywz","v9s","ni","b5","x3","o5o","yj","cc","p0e","ctz","jgn","k2","7h","n","9hm","9mi","zs","v3b","fd","4wx","7aj","xhf","ox5","bqd","n1","vq","7uw","2o","sx","z","c","4cm","k4","ff","r1a","4q4","c6","hgu","04l","ofg","35","tt","wr","ihb","o8w","uf","j5p","oj","hn","_7","6i","ip_","k3","7g4","k7h","u9z","6j","bw","4f","16b","_te","pa","g5j","uh","4y","0f5","d4t","4s","mib","csz","c4y","bv","169","fr","smn","fzr","nv","31","yt2","fj","7","h4u","9zo","78","zpt","l1z","s85","8z","m5","aeu","ey","aj8","if","nt","rp4","qhi","fe","fc5","4","np","r7d","bq6","qwa","9uv","8o","ef","orx","ia","cox","5u","hr1","j_n","gwp","hcl","i1","7fl","6ia","b6","9h","jd","2ks","8m","ib4","xna","ofh","tau","3a","dw","kce","79n","8og","8d","wm_","l","e1u","qum","yd5","yap","pq","r6","hfm","3vj","mo","d28","be","css","2c5","_8s","d9_","ns","j_","ep","jfy","bp","vj","s8","ml","jn","atr","xa","1wn","ok","es","91s","fm","vbo","dc3","qr","v8","k7","oc","g8f","wdy","4z3","on","bl","pck","zu","g4","6p","3ap","5w","5tm","1ct","k0","o_","xrd","7q","p9","08p","se5","mr","a4c","h8","kt","dy","mq","l6","mf","19e","syd","t3","av","pcx","o3","nco","b2l","on2","61v","e0","51v","4b","o9","a7","cx","2oo","98l","4t","9wd","pp9","9_","8w1","18y","1o","2y","amn","80_","ewa","p9p","70u","qf3","m1","7jg","20","oyc","wdq","ic7","79","i95","2s","18t","t1","jrq","gpy","rd","360","zlq","avp","7p","7e","ty_","e9l","p6","a3","qv","hoz","6_t","2h","_f9","gx","kj","1nq","hv","au","nbs","u89","e5","z9d","o9x","lt5","fn","0k","q9","3s","w_","49i","_xj","yfc","omx","yhn","093","4n","mcd","q68","ci","0yh","sfn","4l","ih","55","7le","_dl","3d","swn","_d","z75","99k","jq6","mho","_w5","2f3","lg","2k","tco","ty","0r","yh2","k8i","_u","rks","0s","c90","gv","va","l4r","bt","hys","ca","a99","7g","dz","me1","fv6","i2_","f2","ix","oze","s9a","n5","24","ta","yi","eu","vn","ywt","rvp"
     };
 
     public static String[] getRandomizedAndTrimmedArray(String[] sourceArray) {
@@ -37,6 +37,7 @@ public class RandomArraySelectorTest {
         return tempList.subList(0, Math.min(tempList.size(), 20)).toArray(new String[0]);
     }
 
+
     public static void main(String[] args) {
         RandomArraySelectorTest selector = new RandomArraySelectorTest();
 
@@ -47,5 +48,6 @@ public class RandomArraySelectorTest {
         // Get a randomized array from mAToAll with at least 20 elements
         String[] randomMAToAll = selector.getRandomizedAndTrimmedArray(selector.mAToAll);
         System.out.println("Random mAToAll: " + Arrays.toString(randomMAToAll));
+
     }
 }
